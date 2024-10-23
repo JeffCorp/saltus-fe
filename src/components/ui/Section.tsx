7b20cd63c4bd54aa95ffa7fe146cd9286301bc9f
Tyno <116711205+Tyno1@users.noreply.github.com>
2024-10-23 07:43:53 +0100

    modified navbar adding auth buttons

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -4,14 +4,20 @@ import { FC } from "react";
 interface SectionProps {
   children: React.ReactNode;
   variant?: "primary" | "secondary";
+  classname?: string;
 }
 
-const Section: FC<SectionProps> = ({ children, variant }) => {
+const Section: FC<SectionProps> = ({ children, variant, classname }) => {
   return (
-    <section className={cn("w-full py-12 md:py-24 lg:py-32 xl:py-48 flex justify-center items-center", variant === "primary" ? "bg-white" : "bg-black")}>
+    <section
+      className={cn(
+        `w-full py-12 md:py-24 lg:py-32 xl:py-48 flex justify-center items-center ${classname}`,
+        variant === "primary" ? "bg-white" : "bg-black"
+      )}
+    >
       {children}
     </section>
   );
-}
+};
 
 export default Section;
